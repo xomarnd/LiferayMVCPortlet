b43bb07acfc9a42a26b2001e9686b3de8b30b8cc
av.smirnov <a.smirnov@cometrica.ru>
2021-05-19 11:56:48 +0300

    add ggShell user command

---
+++ modules/SampleMVCPortlet/src/main/java/ru/sample/gogo/UserCountCommand.java
@@ -0,0 +1,22 @@
+package ru.sample.gogo;
+
+import com.liferay.portal.kernel.service.UserLocalService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+		property = {"osgi.command.function=usercount",
+					"osgi.command.scope=blade"},
+		service = Object.class
+)
+public class UserCountCommand {
+
+	@Reference
+	private UserLocalService userLocalService;
+
+	public void usercount() {
+		System.out.println(
+				"Usercount: " + userLocalService.getUsersCount());
+	}
+
+}
